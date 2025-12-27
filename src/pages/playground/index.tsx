2d2d76469d49226b3879f8360541f6f4aa449065
hasenkday <nadia.hasenkday@gmail.com>
2025-12-26 21:53:53 -0300

    testing base components at playground

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,4 +1,14 @@
 import { Button } from '@/components/atoms/button'
+import { Divider } from '@/components/atoms/divider'
+import { InputField } from '@/components/atoms/input-field'
+import { SwitchField } from '@/components/atoms/switch-field'
+import { Tag } from '@/components/atoms/tag'
+import { TextareaField } from '@/components/atoms/textarea-field'
+import { Card } from '@/components/molecules/card'
+import { CheckboxGoup } from '@/components/molecules/checkbox-goup'
+import { Dropdown } from '@/components/molecules/dropdown'
+import { FormField } from '@/components/molecules/form-field'
+import { RadioGroup } from '@/components/molecules/radio-group'
 import { cn } from '@/lib/cn'
 
 export default function PlaygroundPage() {
@@ -14,6 +24,16 @@ export default function PlaygroundPage() {
 
         <div className="flex flex-wrap">
           <Button>Primary</Button>
+          <FormField />
+          <Card />
+          <CheckboxGoup />
+          <Dropdown />
+          <Divider />
+          <RadioGroup />
+          <SwitchField />
+          <TextareaField />
+          <InputField />
+          <Tag />
         </div>
       </div>
     </div>
